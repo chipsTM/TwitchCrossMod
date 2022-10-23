fd2e1a1f6a55ab34914937856766ccd13684a9e1
chips <deafchips0@gmail.com>
2022-10-23 10:20:23 -0400

    migrate to github pages for auth

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,8 @@
     windows_subsystem = "windows"
 )]
 
-#[tokio::main]
-async fn main() {
+fn main() {
     
-    tokio::spawn(async move {
-        warp::serve(warp::fs::dir(""))
-            .run(([127, 0, 0, 1], 7777))
-            .await
-    });
-
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
